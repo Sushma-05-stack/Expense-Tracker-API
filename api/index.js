b2f313bb6e5c2a0e940533498a1edb8474622440
Sushma ennakapally <23eg107b20@anurag.edu.in>
2026-05-20 10:15:04 +0530

    Move backend to api folder for Vercel

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const resourceRoutes = require('./routes/resourceRoutes');
+const resourceRoutes = require('../routes/resourceRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
